ff261c110e6df6d3bf97fa7099d1817f002818d6
Frédéric Haziza <frederic.haziza@crg.eu>
2026-09-25 14:22:52 +0200

    Updating the archiver to use location and skip_backup switch

--- src/ingestion/archiver/archiver/archive.py
+++ src/ingestion/archiver/archiver/archive.py
@@ -48,7 +48,7 @@ async def checkum_and_compare(path, filesize, orgmd, chunksize=1<<23): # 8 MB
         raise ChecksumsNotMatching(path, c, orgmd)
 
 
-async def _do_copy(bufsize, staging_path, vault_path, backup_path):
+async def _do_copy(bufsize, staging_path, destinations):
 
     LOG.info('Copying...') 
     payload_filesize = os.stat(staging_path).st_size
@@ -57,13 +57,13 @@ async def _do_copy(bufsize, staging_path, vault_path, backup_path):
     start_time = time.time()
 
     # Create directories
-    Path(vault_path).parent.mkdir(parents=True, exist_ok=True)
-    Path(backup_path).parent.mkdir(parents=True, exist_ok=True)
+    for f in destinations:
+        Path(f).parent.mkdir(parents=True, exist_ok=True)
 
     chunk = memoryview(bytearray(bufsize))
     bytes_read = 0
 
-    async with AsyncTransfer(staging_path, [vault_path, backup_path]) as t:
+    async with AsyncTransfer(staging_path, destinations) as t:
 
         while True:
 
@@ -89,41 +89,47 @@ async def _do_copy(bufsize, staging_path, vault_path, backup_path):
     payload_sha256_checksum = payload_sha256.hexdigest()
     LOG.debug('Payload checksum: %s', payload_sha256_checksum)
 
-    # We now read the Vault file again
-    LOG.info('Checksuming %s', vault_path)
-    await checkum_and_compare(vault_path,
-                              payload_filesize, payload_sha256_checksum,
-                              chunksize=bufsize)
-
-    # Same for the backup file
-    LOG.info('Checksuming %s', backup_path)
-    await checkum_and_compare(backup_path,
-                              payload_filesize, payload_sha256_checksum,
-                              chunksize=bufsize)
+    # We now read the files again
+    for f in destinations:
+        LOG.info('Checksuming %s', f)
+        await checkum_and_compare(f,
+                                  payload_filesize, payload_sha256_checksum,
+                                  chunksize=bufsize)
 
     # saving info on success
     set_info(vault_path, 'sha256', payload_sha256_checksum)
 
     # Vault and Backup file: read-only (umask already applied)
-    os.chmod(vault_path,
-             os.stat(vault_path).st_mode & 0o444)
-    os.chmod(backup_path, 
-             os.stat(backup_path).st_mode & 0o444)
+    for f in destinations:
+        os.chmod(f, os.stat(f).st_mode & 0o444)
 
 
 async def execute(config, message):
     data = message.parsed
     filepath = data['filepath']
-    username = data['user']
-
     accession_id = data['accession_id']
-        
-    LOG.info('Processing %s: [%s]%s', accession_id, username, filepath)
-        
-    staging_prefix = config.get('staging', 'location', raw=True)
-    staging_path = os.path.join(staging_prefix % username, filepath.lstrip('/'))
-    LOG.debug('Staging path: %s', staging_path)
 
+    location = data.get('location') # fail gracefully
+
+    match location:
+        case 'data':
+            staging_prefix = config.get('data', 'location', raw=True)
+            staging_path = os.path.join(staging_prefix, filepath.lstrip('/'))
+            LOG.info('Processing %s: %s', accession_id, filepath)
+
+        case 'tools':
+            staging_prefix = config.get('tools', 'location', raw=True)
+            staging_path = os.path.join(staging_prefix, filepath.lstrip('/'))
+            LOG.info('Processing %s: %s', accession_id, filepath)
+
+        case _: # ingestion files
+            username = data['user']
+            LOG.info('Processing %s: [%s]%s', accession_id, username, filepath)
+            staging_prefix = config.get('staging', 'location', raw=True)
+            staging_path = os.path.join(staging_prefix % username, filepath.lstrip('/'))
+            LOG.info('Processing %s: [%s]%s', accession_id, username, filepath)
+
+    LOG.debug('Staging path: %s', staging_path)
     if not os.path.isfile(staging_path):
         raise FileNotFoundError(f'Staging path: {filepath} | User: {username}')
     
@@ -135,9 +141,14 @@ async def execute(config, message):
     vault_prefix = config.get('vault', 'location')
     vault_path = os.path.join(vault_prefix, relative_path)
     LOG.debug('Vault path: %s', vault_path)
-    backup_prefix = config.get('backup', 'location')
-    backup_path = os.path.join(backup_prefix, relative_path)
-    LOG.debug('Backup path: %s', backup_path)
+
+    destinations = [vault_path]
+
+    if not config.getboolean('DEFAULT', 'skip_backup', fallback=False):
+        backup_prefix = config.get('backup', 'location')
+        backup_path = os.path.join(backup_prefix, relative_path)
+        LOG.debug('Backup path: %s', backup_path)
+        destinations.append(backup_path)
     
     # In case we handle/receive twice the same message, (or archive
     # the same accession_id for 2 different staging files) we raise an
@@ -152,7 +163,7 @@ async def execute(config, message):
 
     # ... and cue music
     try:
-        await _do_copy(bufsize, staging_path, vault_path, backup_path)
+        await _do_copy(bufsize, staging_path, destinations)
 
         # Success: send completion
         data['type'] = 'archival.completed'
@@ -163,7 +174,7 @@ async def execute(config, message):
     except Exception as e:
         LOG.error('Cleaning on: %r', e)
         # Note: other concurrent messages do not reach that point
-        for f in [vault_path, backup_path]:
+        for f in destinations:
             try:
                 remove_info(f, 'sha256')
                 #remove_info(f, 'decrypted_sha256')
